89aa219d4500c02858e0583cf5f3883e80ade1fb
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-08 17:12:34 +0200

    Update temp_disc.py

--- temp_disc.py
+++ temp_disc.py
@@ -1,57 +1,30 @@
 import torch 
 import torch.nn as nn
-from torch.nn import init
-import torch.nn.functional as F
 import torch.nn.utils.spectral_norm as SpectralNorm
-import functools
-from utils import Attention, DBlock, snconv3d, snlinear
 
 class Discriminator(nn.Module):
-  def __init__(self, params):
-    super(Discriminator, self).__init__()
-    self.p = params
-    # Architecture
-    self.arch = {'in_channels' :  [self.p.filterD*item for item in [1, 2, 4, 8]],
-               'out_channels' : [item * self.p.filterD for item in [2, 4, 8, 16]],
-               'downsample' : [True] * 4 + [False],
-               'resolution' : [32, 16, 8, 4, 4],
-               'attention' : {2**i: 2**i in [int(item) for item in '16'.split('_')]
-                              for i in range(2,7)}}
-    
-    # Prepare model
-    self.input_conv = snconv3d(1, self.arch['in_channels'][0])
-    self.blocks = []
-    for index in range(len(self.arch['out_channels'])):
-      self.blocks += [[DBlock(in_channels=self.arch['in_channels'][index] if d_index==0 else self.arch['out_channels'][index],
-                       out_channels=self.arch['out_channels'][index],
-                       preactivation=True,
-                       downsample=(nn.AvgPool2d(2) if self.arch['downsample'][index] and d_index==0 else None))
-                       for d_index in range(1)]]
-      if self.p.att:
-        if self.arch['attention'][self.arch['resolution'][index]]:
-          self.blocks[-1] += [Attention(self.arch['out_channels'][index])]
+    def __init__(self, params):
+        super(Discriminator, self).__init__()
+        ndf = params.filterD
+        self.main = nn.Sequential(
+            # input is (1) x 64 x 64
+            SpectralNorm(nn.Conv2d(1, ndf, 4, stride=2, padding=1, bias=False)), 
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf) x 32 x 32
+            SpectralNorm(nn.Conv2d(ndf, ndf * 2, 4, stride=2, padding=1, bias=False)),
+            nn.BatchNorm2d(ndf * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*2) x 16 x 16 
+            SpectralNorm(nn.Conv2d(ndf * 2, ndf * 4, 4, stride=2, padding=1, bias=False)),
+            nn.BatchNorm2d(ndf * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*4) x 8 x 8
+            SpectralNorm(nn.Conv2d(ndf * 4, ndf * 8, 4, stride=2, padding=1, bias=False)),
+            nn.BatchNorm2d(ndf * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+            # state size. (ndf*8) x 4 x 4
+        )
 
-    self.blocks = nn.ModuleList([nn.ModuleList(block) for block in self.blocks])
-    #self.linear = snlinear(self.arch['out_channels'][-1], 1)
-    #self.linear2 = nn.Linear(self.arch['out_channels'][-1],1)
-    self.activation = nn.ReLU(inplace=True)
-    self.init_weights()
-
-  def init_weights(self):
-    self.param_count = 0
-    for module in self.modules():
-      if (isinstance(module, nn.Conv3d)
-          or isinstance(module, nn.Linear)):
-        init.orthogonal_(module.weight)
-        self.param_count += sum([p.data.nelement() for p in module.parameters()])
-    print('Param count for D''s initialized parameters: %d' % self.param_count)
-
-  def forward(self, x):
-    # Run input conv
-    h = self.input_conv(x)
-    for index, blocklist in enumerate(self.blocks):
-      for block in blocklist:
-        h = block(h)
-    # Apply global sum pooling as in SN-GAN
-    h = torch.sum(self.activation(h), [2, 3])
-    return h#self.linear(h)
+    def forward(self, input):
+        out = self.main(input)
+        return torch.sum(out, [2, 3])
